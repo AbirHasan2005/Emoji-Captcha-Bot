81dd7e53d6343378888bec25da229528a7ab24a3
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2022-01-05 09:11:55 +0530

    use uuid lib instead random lib for file name(id)

--- helpers/generate_id.py
+++ helpers/generate_id.py
@@ -1,14 +1,7 @@
 # (c) @AbirHasan2005
 
-import random
-
-__chars = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z " \
-          "a b c d e f g h i j k l m n o p q r s t u v w x y z " \
-          "1 2 3 4 5 6 7 8 9 0".split()
+import uuid
 
 
 def generate_rnd_id():
-    __id = "fa"
-    for i in range(15):
-        __id += f"{random.choice(__chars)}"
-    return __id
+    return str(uuid.uuid4().hex)
