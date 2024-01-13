83f53193a0508e47c3ea1bfd2fd307a10611c1e6
SI_NN_ER_LS <72606082+si-nn-er-ls@users.noreply.github.com>
2024-01-13 13:10:25 +0530

    fix

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ logging.basicConfig(
 )
 
 CaptchaBot = Client(
-    session_name=Config.SESSION_NAME,
+    Config.SESSION_NAME,
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN
